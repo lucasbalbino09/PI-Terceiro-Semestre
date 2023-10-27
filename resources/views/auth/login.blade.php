f2dab6cfd0487635d45d589bdf12aa3168d3e21a
lucas.balbino <balbino2409@gmail.com>
2023-10-27 13:07:07 -0300

    subindo uma alteracao do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,41 +14,43 @@
     <form method="POST" action="{{ route('login') }}">
 
         @csrf
-
-
-    <div class="login">
+        <div class="login">
         <div class="logo-yyt-logo-lo">
             <img class="logo-yyt-logo-login" src="/img/logoOficialOficial.png">
         </div>
         <!-- Email Address -->
-        <div>
-            <label for="email">Email </label>
-            <input class="inputLogin" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
-        </div>
-
-        <!-- Password -->
-        <div>
-            <label for="password">Senha </label>
-            <input id="password" class="inputLogin"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="senhmiy">
-
-        </div>
+        <div class="formulario">
             <div>
-                <button class="buttonLogin">
-                    login
-                </button>
-
-                <a class="link-yut" href="/register">
-                    Cadastrar
-                </a>
+                <label for="email" class="label-email">Email: </label>
+                <input class="inputLogin" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
             </div>
+    
+            <!-- Password -->
+            <div>
+                <label for="password" class="label-senha">Senha: </label>
+                <input id="password" class="inputLogin"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
+    
+            <!-- Remember Me -->
+            <div class="senhmiy">
+    
+            </div>
+                <div>
+                    <button class="buttonLogin">
+                        login
+                    </button>
+    
+                    <a class="link-yut" href="/register">
+                        Cadastrar
+                    </a>
+                </div>
+            </div>
+
         </div>
+        
 
     </form>
 </div>
