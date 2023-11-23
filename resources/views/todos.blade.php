0e8cd0a12ef6ae885e5cce05f07e6004a725fc53
lucas.balbino <balbino2409@gmail.com>
2023-11-23 09:43:26 -0300

    validadores de imagerm

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -9,10 +9,10 @@
 
 </style>
 <h1>Todos os nossos produtos</h1>
-@foreach($produtos as $produto)
+{{-- @foreach($produtos as $produto)
 
     <div class="card" style="width: 30rem;">
-                        @if (count($produto->ProdutoImagem)>0 )
+                        @if (count($produto->ProdutoImagem) > 0 && !empty($produto->ProdutoImagem[0]->IMAGEM_URL) )
                         <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
                         @else
                         <img src="../img/indisponivel.jpg" class="card-img-top" alt="...">
@@ -24,8 +24,38 @@
                         </div>
                     </div>
 
+@endforeach --}}
 
+@foreach($produtos as $produto)
+    <div class="card" style="width: 30rem;">
+        @if (count($produto->ProdutoImagem) > 0 && !empty($produto->ProdutoImagem[0]->IMAGEM_URL))
+            @php
+                $imagemUrl = $produto->ProdutoImagem[0]->IMAGEM_URL;
 
+                // Verifica se a imagem é válida (não retorna 404)
+                $headers = @get_headers($imagemUrl);
 
+                // Se a variável $headers for falsa ou contiver '404', consideramos a imagem inválida
+                $imagemValida = $headers && !in_array('HTTP/1.1 404 Not Found', $headers) && !in_array('HTTP/1.1 403 Not Found', $headers);
+            @endphp
+
+            @if ($imagemValida)
+                <img src="{{ $imagemUrl }}" class="card-img-top" alt="...">
+            @else
+                <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+            @endif
+        @else
+            <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+        @endif
+        <div class="card-body">
+            <h5 class="card-title">{{ $produto->PRODUTO_NOME }}</h5>                            
+            <p class="card-text">R${{ $produto->PRODUTO_PRECO }}</p>
+            <a href="{{ route('produto.show', $produto->PRODUTO_ID) }}">
+                <button type="submit" class="buttonLogin">Comprar</button>
+            </a>
+        </div>
+    </div>
 @endforeach
+
+
  @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,26 @@
 
         <div class="col">
             <div class="card">
-                <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
+                {{-- <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="..."> --}}
+                @if (count($produto->ProdutoImagem) > 0 && !empty($produto->ProdutoImagem[0]->IMAGEM_URL))
+                @php
+                    $imagemUrl = $produto->ProdutoImagem[0]->IMAGEM_URL;
+    
+                    // Verifica se a imagem é válida (não retorna 404)
+                    $headers = @get_headers($imagemUrl);
+    
+                    // Se a variável $headers for falsa ou contiver '404', consideramos a imagem inválida
+                    $imagemValida = $headers && !in_array('HTTP/1.1 404 Not Found', $headers) && !in_array('HTTP/1.1 403 Not Found', $headers);
+                @endphp
+    
+                @if ($imagemValida)
+                    <img src="{{ $imagemUrl }}" class="card-img-top" alt="...">
+                @else
+                    <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+                @endif
+            @else
+                <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+            @endif
                 <div class="card-body">
                     <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
                     <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
@@ -73,11 +92,25 @@
 
                 <div class="col">
                     <div class="card">
-                        @if (count($produto->ProdutoImagem)>0 )
-                        <img src="{{$produto->ProdutoImagem[0]->IMAGEM_URL}}" class="card-img-top" alt="...">
-                        @else
-                        <img src="../img/indisponivel.jpg" class="card-img-top" alt="...">
-                        @endif
+                        @if (count($produto->ProdutoImagem) > 0 && !empty($produto->ProdutoImagem[0]->IMAGEM_URL))
+            @php
+                $imagemUrl = $produto->ProdutoImagem[0]->IMAGEM_URL;
+
+                // Verifica se a imagem é válida (não retorna 404)
+                $headers = @get_headers($imagemUrl);
+
+                // Se a variável $headers for falsa ou contiver '404', consideramos a imagem inválida
+                $imagemValida = $headers && !in_array('HTTP/1.1 404 Not Found', $headers) && !in_array('HTTP/1.1 403 Not Found', $headers);
+            @endphp
+
+            @if ($imagemValida)
+                <img src="{{ $imagemUrl }}" class="card-img-top" alt="...">
+            @else
+                <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+            @endif
+        @else
+            <img src="{{ asset('img/indisponivel.jpg') }}" class="card-img-top" alt="Imagem Indisponível">
+        @endif
                         <div class="card-body">
                             <h5 class="card-title">{{$produto->PRODUTO_NOME}}</h5>
                             <p class="card-text">R${{$produto->PRODUTO_PRECO}}</p>
